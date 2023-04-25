510fe12da82bbf9cde9fa59e8a77cac3c1e8e314
pabloccard <pabloccard@gmail.com>
2023-04-24 21:18:47 -0300

    testando erro no build

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -5,7 +5,7 @@ import { Container } from './styles'
 export const DefaultLayout = () => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Container>
         <Outlet />
       </Container>
